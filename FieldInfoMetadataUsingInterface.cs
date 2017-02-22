571141e6bf6a38be48403f0539e9677c3566d5f0
Yehudah alt <yehudah.alt@gmail.com>
2017-02-22 18:19:37 +0200

    txt => cs

---
+++ FieldInfoMetadataUsingInterface.cs
@@ -0,0 +1,31 @@
+static class PivotFieldInfoHelper
+{
+    // This code is better.
+    private static TAtt GetAttributeUsingInterface<TAtt>(this IPivotFieldInfo fieldInfo) where TAtt : Attribute
+    {
+        // This is for PropertyInfoFieldInfo and QueryableFieldDescription
+        if (fieldInfo is IPropertyInfoSourceFieldInfo)
+        {
+            PropertyInfo propertyInfo = (fieldInfo as PropertyInfoFieldInfo).PropertyInfo;
+            return propertyInfo.GetCustomAttribute<TAtt>();
+        }
+
+        // this is for DateTimePropertyFieldInfo, which is wrapper for PivotFieldInfo
+        if (fieldInfo is IPivotFieldSourceFieldInfo)
+        {
+            IPivotFieldInfo innerFieldInfo = (fieldInfo as IPivotFieldSourceFieldInfo).FieldInfo;
+            return innerFieldInfo.GetAttributeUsingInterface<TAtt>();
+        }
+
+        // This is for PropertyDescriptorFieldInfo
+        if (propertyDescriptorField is IPropertyDescriptorFieldInfo)
+        {
+            return (propertyDescriptorField as IPropertyDescriptorFieldInfo).PropertyDescriptor
+                .Attributes
+                .OfType<TAtt>()
+                .FirstOrDefault();
+        }
+
+        return null;
+    }
+}
